86af8709e9c816aea6618c4cb8f6ead9a288df50
Abhijit <abhijeetpavse7570@gmail.com>
2026-07-18 18:47:18 +0530

    first commit1

---
+++ basic.py
@@ -0,0 +1,82 @@
+# print("hello my name is Abhijit and\nI am learning Python programming.")
+
+# x=5
+# y=2
+
+# print("The Value of x is:", x)
+# print("The Value of y is:", y)
+
+# z= x + y
+# print("The Value of z is:", z)
+
+# print((z)*7)
+
+# #or 
+
+# #print("x + y =", x + y)
+
+# # both can be used to print the value of z. The 
+# # first method is to store the value in a variable and then print it, 
+# # while the second method is to directly print the result of the addition operation.
+
+# # subtraction operation
+# print("the subtraction of x and y is:", x - y)
+
+# print("the subtraction of", x, "and", y, "is:", x - y)
+
+# # multiplication operation
+# print("the multiplication of x and y is:", x * y)
+
+# # division operation
+# print("the division of x and y is:", x / y)
+
+# # modulus operation
+# print("the modulus of x and y is:", x % y)
+
+# # exponentiation operation
+# print("the exponentiation of x and y is:", x ** y)
+
+# # floor division operation
+# print("the floor division of x and y is:", x // y)
+
+
+# emp_id = input("Enter your employee id: ")
+# emp_name = input("Enter your employee name: ")
+# print('welcome', emp_id, emp_name)
+
+
+x=int(input("x: "))  #int will convert the input string into integer 
+y=float(input("y: ")) #float will convert the input string into float (decimal) number
+
+print("The Value of x is:", x)
+print("The Value of y is:", y)
+
+
+# #addition operation
+# print("the addition of x and y is:", x + y)
+
+# # subtraction operation
+# print("the subtraction of x and y is:", x - y)
+
+# print("the subtraction of", x, "and", y, "is:", x - y)
+
+# # multiplication operation
+# print("the multiplication of x and y is:", x * y)
+
+# # division operation
+# print("the division of x and y is:", x / y)
+
+# # modulus operation
+# print("the modulus of x and y is:", x % y)
+
+# # exponentiation operation
+# print("the exponentiation of x and y is:", x ** y)
+
+# # floor division operation
+# print("the floor division of x and y is:", x // y)
+
+print("data type of x is:", type(x))
+print("data type of y is:", type(y))
+
+print(f"the addition of {x} and {y} is: {x + y}") # f is used to format the string and print the value of x and y in the string.
+ 
---
+++ basic_print.py
@@ -0,0 +1,10 @@
+print("hi guys", end=" ")
+print("welcome to python programming")
+
+x=input("Date: ")
+y=input("Month: ")
+z=input("Year: ")
+
+print("Date:" ,end="") #after this line 'end' the cursor will remain in the same line 
+print(x,y,z,sep="/") # / is used as a separator between the values of x, y, and z. 
+
